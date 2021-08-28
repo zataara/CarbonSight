2ce90efa02f8e9862b935a3d9071cf8df85a56e0
zataara <bryantmac@gmail.com>
2021-08-28 00:05:25 -0700

    added following table

--- models.py
+++ models.py
@@ -87,6 +87,12 @@ artist_user = db.Table('artist_user',
     db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
     db.Column('artist_id', db.Integer, db.ForeignKey('artist.id'), primary_key=True))
 
+following = db.Table('following',
+    db.Column('followee_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id'), primary_key=True))
+
+
+
 
 
 
