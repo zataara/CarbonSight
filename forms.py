300b0e05d21fa2daef1f6be31178d45b2b4d92d1
zataara <bryantmac@gmail.com>
2021-10-27 13:55:50 -0700

    added form for gathering Home Usage information

--- forms.py
+++ forms.py
@@ -11,6 +11,7 @@ class UserForm(FlaskForm):
     email = StringField('Email Address', validators=[InputRequired(), Length(max=50)])
     first_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
     last_name = StringField('Last Name', validators=[InputRequired(), Length(max=30)])
+    state = SelectField('State', choices=[("al", "Alabama"), ("ak", "Alaska"), ("az", "Arizona"), ("ar", "Arkansas"), ("ca", "California"), ("co", "Colorado"), ("ct", "Connecticut"), ("cd", "Washington D.C."), ("de", "Delaware"), ("fl", "Florida"), ("ga", "Georgia"), ("hi", "Hawaii"), ("id", "Idaho"), ("il", "Illinois"), ("in", "Indiana"), ("ia", "Iowa"), ("ks", "Kansas"), ("ky", "Kentucky"), ("la", "Louisiana"), ("me", "Maine"), ("md", "Maryland"), ("ma", "Massachusetts"), ("mi", "Michigan"), ("mn", "Minnesota"), ("ms", "Missouri"), ("MO", "Montana"), ("mt", "Montana"), ("ne", "Nebraska"), ("nv", "Nevada"), ("nh", "New Hampshire"), ("nj", "New Jersey"), ("nm", "New Mexico"), ("ny", "New York"), ("nc", "North Carolina"), ("nd", "North Dakota"), ("oh", "Ohio"), ("ok", "Oklahoma"), ("or", "Oregon"), ("pa", "Pennsylvania"), ("ri", "Rhode Island"), ("sc", "South Carolina"), ("sd", "South Dakota"), ("tn", "Tennessee"), ("tx", "Texas"), ("ut", "Utah"), ("vt", "Vermont"), ("va", "Virginia"), ("wa", "Washington"), ("wv", "West Virginia"), ("wi", "Wisconson"), ("wy", "Wyoming")])
 
 
 class LoginForm(FlaskForm):
@@ -19,7 +20,6 @@ class LoginForm(FlaskForm):
 
 class HomeUsageForm(FlaskForm):
     homename = StringField('Home Name', validators=[InputRequired(), Length(max=30)])
-    homestate = SelectField('State', choices=[("al", "Alabama"), ("ak", "Alaska"), ("az", "Arizona"), ("ar", "Arkansas"), ("ca", "California"), ("co", "Colorado"), ("ct", "Connecticut"), ("cd", "Washington D.C."), ("de", "Delaware"), ("fl", "Florida"), ("ga", "Georgia"), ("hi", "Hawaii"), ("id", "Idaho"), ("il", "Illinois"), ("in", "Indiana"), ("ia", "Iowa"), ("ks", "Kansas"), ("ky", "Kentucky"), ("la", "Louisiana"), ("me", "Maine"), ("md", "Maryland"), ("ma", "Massachusetts"), ("mi", "Michigan"), ("mn", "Minnesota"), ("ms", "Missouri"), ("MO", "Montana"), ("mt", "Montana"), ("ne", "Nebraska"), ("nv", "Nevada"), ("nh", "New Hampshire"), ("nj", "New Jersey"), ("nm", "New Mexico"), ("ny", "New York"), ("nc", "North Carolina"), ("nd", "North Dakota"), ("oh", "Ohio"), ("ok", "Oklahoma"), ("or", "Oregon"), ("pa", "Pennsylvania"), ("ri", "Rhode Island"), ("sc", "South Carolina"), ("sd", "South Dakota"), ("tn", "Tennessee"), ("tx", "Texas"), ("ut", "Utah"), ("vt", "Vermont"), ("va", "Virginia"), ("wa", "Washington"), ("wv", "West Virginia"), ("wi", "Wisconson"), ("wy", "Wyoming")])
     month = StringField('Month', validators=[InputRequired(), Length(max=30)])
     usage = IntegerField('Usage', validators=[InputRequired(), NumberRange(min=1, max=None)])
 
