2bc0665ff81af9e9163284bf354e2364c0f110a0
zataara <bryantmac@gmail.com>
2021-10-28 17:55:19 -0700

    edited User and Home usage models for db

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+"""SQLAlchemy models for CarbonSight"""
+
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
@@ -18,25 +21,28 @@ class User(db.Model):
     def __repr__(self):
         
         u = self
-        return f'<User {u.id} username={u.username} name={first_name} {last_name}>'
+        return f"<User {u.username}>"
 
-    id = db.Column(db.Integer,
-                    primary_key=True,
-                    autoincrement=True)
-    email = db.Column(db.String(20),
+    username = db.Column(db.String(20),
+                            primary_key=True,
                             nullable=False,
                             unique=True)
-    password = db.Column(db.String,
-                            nullable=False)
-    first_name = db.Column(db.String(30),
-                            nullable=False)
-    last_name = db.Column(db.String(30),
-                            nullable=False)
-    state = db.Column(db.String(2),
-                            nullable=False) 
+    password = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, nullable=False)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    state = db.Column(db.String(2), nullable=False)
 
     @classmethod
-    def register(cls, username, password, email, first_name, last_name):
+    def sum_electric_usage(self):
+        out = 0
+
+        for row in self.homeusage:
+            out = row.usage + out
+        return 555
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name, state):
         """Register user with a hashed password and return that user"""
         
         hashed = bcrypt.generate_password_hash(password)
@@ -47,7 +53,8 @@ class User(db.Model):
             password=hashed_utf8,
             email=email,
             first_name=first_name,
-            last_name=last_name
+            last_name=last_name,
+            state=state
         )
         db.session.add(new_user)
         return new_user
@@ -62,13 +69,24 @@ class User(db.Model):
             return u
         else:
             return False
+    
+    homeusage = db.relationship('HomeUsage', backref='user', cascade='all,delete')
 
 
 
 class HomeUsage(db.Model):
     '''Database Model for Home Usage'''
 
-    __tablename__ = 'HomeUsage'
+    __tablename__ = 'homeusage'
+
+    def __init__(self, username, month_name, usage, carbon_g, carbon_lb, carbon_kg, carbon_mt):
+        self.username = username
+        self.month_name = month_name
+        self.usage = usage
+        self.carbon_g = carbon_g
+        self.carbon_lb = carbon_lb
+        self.carbon_kg = carbon_kg
+        self.carbon_mt = carbon_mt
 
     def __repr__(self):
         
@@ -78,13 +96,16 @@ class HomeUsage(db.Model):
     id = db.Column(db.Integer,
                     primary_key=True,
                     autoincrement=True)
-    username = db.Column(db.Integer(20),
+    username = db.Column(db.String(20),
                             db.ForeignKey('user.username'),
                             nullable=False,)
     month_name = db.Column(db.String(50),
                             nullable=False)
-    usage = db.Column(db.Integer(10),
+    usage = db.Column(db.Integer,
                             nullable=False)
-
+    carbon_g = db.Column(db.Integer, nullable = True)
+    carbon_lb = db.Column(db.Integer, nullable = True)
+    carbon_kg = db.Column(db.Integer, nullable = True)
+    carbon_mt = db.Column(db.Integer, nullable = True)
 
 
