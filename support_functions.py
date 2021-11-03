1c2fcd6ce5ac214dd73fc798e1218500e332cd43
zataara <bryantmac@gmail.com>
2021-11-02 23:10:36 -0700

    deleting unused files

--- support_functions.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, session
-from werkzeug.exceptions import Unauthorized
-
-
-def checkUserId(username):
-
-    if 'username' not in session or username != session['username']:
-        raise Unauthorized()
