0a216c001e4aecc13fe49b1ec1ee8774306edee9
zataara <bryantmac@gmail.com>
2021-11-03 13:28:31 -0700

    adding funtionality for deleting a monthly usage amount

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,32 @@
 
 
 const removeHomeUsageButtons = document.querySelectorAll('.rhu')
+const removeVehicleUsageButtons = document.querySelectorAll('.rvu')
 
-
+// Delete a Home Usage
 for(let btn of removeHomeUsageButtons) {
     btn.addEventListener('click', function(e) {
-        deleteUsage(e.target.parentElement.parentElement.id)
+        deleteHomeUsage(e.target.parentElement.parentElement.id)
         
         e.target.parentElement.parentElement.remove()
     })
 }
 
-async function deleteUsage(id) {
+async function deleteHomeUsage(id) {
     await axios.post(`/homeUsage/${id}/delete`)
+}
+
+// Delete Vehicle Usage
+for(let btn of removeVehicleUsageButtons) {
+    btn.addEventListener('click', function(e) {
+        deleteVehicleUsage(e.target.parentElement.parentElement.id)
+        
+        e.target.parentElement.parentElement.remove()
+    })
+}
+
+async function deleteVehicleUsage(id) {
+    await axios.post(`/vehicleUsage/${id}/delete`)
 }
 
 
