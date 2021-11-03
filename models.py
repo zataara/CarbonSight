50558df0d8c4dcfb97bcee1bdfc5eb4f2112cadb
zataara <bryantmac@gmail.com>
2021-11-02 23:10:58 -0700

    editing Vehicle class

--- models.py
+++ models.py
@@ -36,6 +36,7 @@ class User(db.Model):
     @classmethod
     def sum_electric_usage(self):
         out = 0
+        # import code; code.interact(local=dict(globals(), **locals()))
 
         for row in self.homeusage:
             out = row.usage + out
@@ -73,6 +74,7 @@ class User(db.Model):
     homeusage = db.relationship('HomeUsage', backref='user', cascade='all,delete')
 
     vehicle = db.relationship('Vehicle', backref='user', cascade='all,delete')
+
 
 
 
@@ -116,26 +118,66 @@ class Vehicle(db.Model):
 
     __tablename__ = 'vehicle'
 
-    def __init__(self, make, model, year):
-        self.id = id
+    def __init__(self, username, name, make, model, year, carbon_interface_id):
+        self.username = username
+        self.name = name
         self.make = make
         self.model = model
         self.year = year
+        self.carbon_interface_id = carbon_interface_id
         
     def __repr__(self):
         
         v = self
-        return f'<Vehicle>'
+        return f'<Vehicle Make:${v.make}, Model:${v.model}, Year:{v.year}>'
 
-    id = db.Column(db.String,
+    id = db.Column(db.Integer,
                     primary_key=True,
-                    autoincrement=True,)
+                    autoincrement=True)
     username = db.Column(db.String(20),
                             db.ForeignKey('user.username'),
-                            nullable=False,)
-    make = db.Column(db.String(50),
                             nullable=False)
-    model = db.Column(db.Integer,
+    name = db.Column(db.String(30),
                             nullable=False)
-    year = db.Column(db.Integer, nullable = True)
+    make = db.Column(db.String(30),
+                            nullable=False)
+    model = db.Column(db.String(30),
+                            nullable=False)
+    year = db.Column(db.Integer, nullable=False)
     carbon_interface_id = db.Column(db.String(), nullable=False)
+
+    # vehicleusage = db.relationship('VehicleUsage', backref='vehicle', cascade='all,delete')
+
+
+
+# class VehicleUsage(db.Model):
+#     '''Database Model for a Vehicles Usage'''
+
+#     __tablename__ = 'vehicleusage'
+
+#     def __init__(self, vehicle_id, month_name, mileage, carbon_g, carbon_lb, carbon_kg, carbon_mt):
+#         self.vehicle_id = vehicle_id
+#         self.month_name = month_name
+#         self.mileage = mileage
+#         self.carbon_g = carbon_g
+#         self.carbon_lb = carbon_lb
+#         self.carbon_kg = carbon_kg
+#         self.carbon_mt = carbon_mt
+        
+#     def __repr__(self):
+        
+#         v = self
+#         return f'<VehicleUsage Month:${v.month_name}, >'
+
+#     id = db.Column(db.Integer,
+#                     primary_key=True,
+#                     autoincrement=True,)
+#     vehicle_id = db.Column(db.String(20),
+#                             db.ForeignKey('vehicle.carbon_interface_id'),
+#                             nullable=False)
+#     month_name = db.Column(db.String(50), nullable=False)
+#     mileage = db.Column(db.Integer, nullable=False)
+#     carbon_g = db.Column(db.Integer, nullable=True)
+#     carbon_lb = db.Column(db.Integer, nullable=True)
+#     carbon_kg = db.Column(db.Integer, nullable=True)
+#     carbon_mt = db.Column(db.Integer, nullable=True)
