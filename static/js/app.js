7590378266b16490688097dad02431a3f7d75774
zataara <bryantmac@gmail.com>
2021-11-04 13:24:34 -0700

    added event listeners for deleting flights

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,7 @@
 
 const removeHomeUsageButtons = document.querySelectorAll('.rhu')
 const removeVehicleUsageButtons = document.querySelectorAll('.rvu')
+const removeFlightButtons = document.querySelectorAll('.rf')
 
 // Delete a Home Usage
 for(let btn of removeHomeUsageButtons) {
@@ -24,9 +25,22 @@ for(let btn of removeVehicleUsageButtons) {
         e.target.parentElement.parentElement.remove()
     })
 }
-
 async function deleteVehicleUsage(id) {
     await axios.post(`/vehicleUsage/${id}/delete`)
+}
+
+
+
+// Delete Flight
+for(let btn of removeFlightButtons) {
+    btn.addEventListener('click', function(e) {
+        deleteFlight(e.target.parentElement.parentElement.id)
+        
+        e.target.parentElement.parentElement.remove()
+    })
+}
+async function deleteFlight(id) {
+    await axios.post(`/flight/${id}/delete`)
 }
 
 
