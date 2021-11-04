e4ef905ad87537bbc0523c03a39684c0aee23dac
zataara <bryantmac@gmail.com>
2021-11-04 13:24:55 -0700

    added form for creating a flight

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SelectField, IntegerField, BooleanField
 from wtforms.validators import InputRequired, Optional, Length, NumberRange, URL, EqualTo
+from library import airports, cars, states
 
 
 
@@ -11,7 +12,7 @@ class UserForm(FlaskForm):
     email = StringField('Email Address', validators=[InputRequired(), Length(max=50)])
     first_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
     last_name = StringField('Last Name', validators=[InputRequired(), Length(max=30)])
-    state = SelectField('State', choices=[("al", "Alabama"), ("ak", "Alaska"), ("az", "Arizona"), ("ar", "Arkansas"), ("ca", "California"), ("co", "Colorado"), ("ct", "Connecticut"), ("cd", "Washington D.C."), ("de", "Delaware"), ("fl", "Florida"), ("ga", "Georgia"), ("hi", "Hawaii"), ("id", "Idaho"), ("il", "Illinois"), ("in", "Indiana"), ("ia", "Iowa"), ("ks", "Kansas"), ("ky", "Kentucky"), ("la", "Louisiana"), ("me", "Maine"), ("md", "Maryland"), ("ma", "Massachusetts"), ("mi", "Michigan"), ("mn", "Minnesota"), ("ms", "Missouri"), ("MO", "Montana"), ("mt", "Montana"), ("ne", "Nebraska"), ("nv", "Nevada"), ("nh", "New Hampshire"), ("nj", "New Jersey"), ("nm", "New Mexico"), ("ny", "New York"), ("nc", "North Carolina"), ("nd", "North Dakota"), ("oh", "Ohio"), ("ok", "Oklahoma"), ("or", "Oregon"), ("pa", "Pennsylvania"), ("ri", "Rhode Island"), ("sc", "South Carolina"), ("sd", "South Dakota"), ("tn", "Tennessee"), ("tx", "Texas"), ("ut", "Utah"), ("vt", "Vermont"), ("va", "Virginia"), ("wa", "Washington"), ("wv", "West Virginia"), ("wi", "Wisconson"), ("wy", "Wyoming")])
+    state = SelectField('State', choices=states)
 
 
 class LoginForm(FlaskForm):
@@ -24,7 +25,7 @@ class HomeUsageForm(FlaskForm):
 
 class VehicleForm(FlaskForm):
     year = IntegerField('Year', validators=[InputRequired(), NumberRange(min=1900, max=2025)])
-    make = SelectField('Make', choices=[("Acura"), ("Alfa Romeo"), ("Aston Martin"), ("Audi"), ("Bentley"), ("BMW"), ("Bugatti"), ("Buick"), ("Cadillac"), ("Chevrolet"), ("Chrysler"), ("Dodge"), ("Ferrari"), ("Fiat"), ("Ford"), ("General Motors"), ("GMC"),  ("Honda"), ("Hyundai"), ("Infiniti"), ("Isuzu"),("Jaguar"), ("Jeep"), ("Kia"), ("Koenigsegg"),  ("Lamborghini"), ("Land Rover"), ("Lexus"), ("Lincoln"), ("Lotus"), ("Maserati"), ("Mazda"), ("Mercedes Benz"), ("Mercury"),  ("Mini"), ("Mitsubishi"), ("Nissan"), ("Plymouth"), ("Pontiac"),   ("Porsche"), ("Ram"), ("Rolls Royce"), ("Saab"), ("Saturn"), ("Shelby"), ("Smart"), ("Subaru"), ("Suzuki"), ("Toyota"), ("Tesla"), ("Volkswagon"), ("Volvo")])
+    make = SelectField('Make', choices=cars)
     model = StringField('Model', validators=[InputRequired(), Length(max=20)])
     name = StringField('Name Your Vehicle', validators=[InputRequired(), Length(max=20)])
 
@@ -37,5 +38,16 @@ class VehicleUsageForm(FlaskForm):
     extra_mileage = IntegerField('Extra Monthly Mileage')
 
 
+class FlightForm(FlaskForm):
+    passengers = IntegerField('Passengers',validators=[InputRequired(), NumberRange(min=1, max=4)])
+    departure = SelectField('Departure City', choices=airports)
+    leg1 = SelectField('Stop 1 (Optional)', choices=airports)
+    leg2 = SelectField('Stop 2 (Optional)', choices=airports)
+    destination = SelectField('Departure City', choices=airports)
 
+    
+    
+    
+    
+    
 
